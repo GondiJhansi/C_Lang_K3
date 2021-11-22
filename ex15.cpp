d0b9d06533b836efbf0b34a04b79981294dba39c
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-11-22 14:30:35 +0530

    Add files via upload

---
+++ ex15.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float x = 2.1;
+    double y=2.1;
+    if(x == y)
+    {
+        printf("Hai");
+    }
+    else
+    {
+    	printf("Bye");
+	}
+	return 0;
+}
