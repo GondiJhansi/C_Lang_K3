9e55e200888179eea14bae3718f4fea8dd710024
Jhansi <84505647+GondiJhansi@users.noreply.github.com>
2021-11-28 23:49:11 +0530

    Add files via upload

---
+++ ex91.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	void swap(); 
+	int x = 5, y = 10;
+	swap(&x, &y);
+	printf("x = %d y = %d",x,y);
+	return 0;
+}
+void swap(int *a, int *b)
+{
+	*a ^= *b, *b ^= *a, *a ^= *b;
+}
